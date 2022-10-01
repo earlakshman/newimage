95287648af47c7b7ef62fce0fde7e8770d4efbbb
earlakshman <earlakshmanvta@gmail.com>
2022-10-01 16:23:54 +0530

    Initial commit

---
+++ page2.php
@@ -0,0 +1,44 @@
+<?php 
+session_start();
+
+
+// If upload button is clicked ...
+$_SESSION['filename'] = $_FILES["uploadfile"]["name"];
+$tempname = $_FILES["uploadfile"]["tmp_name"];
+$folder = "./image/" . $_SESSION['filename'];
+
+// Now let's move the uploaded image into the folder: image
+
+move_uploaded_file($tempname, $folder);
+
+	
+
+?>
+  <!-- bootstrap connect start -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <!-- bootstrap connect end -->
+<!-- <link href="index3.css" rel="stylesheet"> -->
+<style>
+    .container{
+			max-width:700px;
+			background-color: gray;
+		}
+</style>
+<div class="container">   
+		<?php
+		
+
+            echo "<div class='responsive'>
+                <div class='gallery'>
+                
+                <img src='./image/".$_SESSION['filename']."' width='600' height='400'>
+                
+           
+            </div>
+            </div>";
+
+		
+		?>
+	</div.container>
